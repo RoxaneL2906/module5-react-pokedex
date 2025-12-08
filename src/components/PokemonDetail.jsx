b011f790f54eea26b96dd5ba50c819f9d86d845e
Roxane Lelievre <x-roxane.lelievre@hotmail.fr>
2025-12-08 09:17:09 +0100

    pokedex fini

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -2,14 +2,38 @@ import "./PokemonDetail.css";
 import { Pokemon } from "./Pokemon";
 
 export function PokemonDetail({ pokemon, evolutions, onClickPokemon }) {
+  function Evolutions() {
+    console.log(evolutions)
+    if (evolutions?.length > 0) {
+      return (
+        <div className="pokemon_evolutions">
+          <p className="pokemon_evolutions_title">
+            {evolutions?.length > 1 ? "Evolutions" : "Evolution"}
+          </p>
+          <div className="pokemon_evolutions_list">
+            {evolutions?.map((evolution) => (
+              <Pokemon
+                pokemon={evolution}
+                key={evolution.id}
+                onClickPokemon={(evolution) => onClickPokemon(evolution)}
+              />
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div className="pokemon_details">
       <div className="pokemon_detail">
         <p className="pokemon_id">n°{pokemon.id}</p>
         <img className="pokemon_image" src={pokemon.image} alt={pokemon.name} />
-        <p className="pokemon_name">{pokemon.name}</p>
+        <h2 className="pokemon_name">{pokemon.name}</h2>
         <div className="types">
-          <p>Types</p>
+          <p>{pokemon.apiTypes.length > 1 ? "Types" : "Type"}</p>
           <div className="types_images">
             {pokemon.apiTypes.map((type) => (
               <img
@@ -22,18 +46,7 @@ export function PokemonDetail({ pokemon, evolutions, onClickPokemon }) {
           </div>
         </div>
       </div>
-      <div className="pokemon_evolutions">
-        <p className="pokemon_evolutions_title">Evolutions</p>
-        <div className="pokemon_evolutions_list">
-          {evolutions?.map((evolution) => (
-            <Pokemon
-              pokemon={evolution}
-              key={evolution.id}
-              onClickPokemon={(evolution) => onClickPokemon(evolution)}
-            />
-          ))}
-        </div>
-      </div>
+      <Evolutions />
     </div>
   );
 }
